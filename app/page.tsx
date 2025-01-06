80703b38c19580001ed583b0727d050118cfb010
Muhammad Hassan <96192977+Hassanshigri@users.noreply.github.com>
2025-01-06 21:02:11 +0500

    hello fyp

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Hello Hassan</h1>
+    </main>
+  )
+}
+
